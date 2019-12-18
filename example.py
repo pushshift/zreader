48626c3bb36f33ec02d79d94f62c0458b8e928b8
root <root@pluto.home>
2019-12-18 22:03:18 +0000

    Change zreader var to reader

--- example.py
+++ example.py
@@ -6,10 +6,10 @@ import ujson as json
 
 
 # Adjust chunk_size as necessary -- defaults to 16,384 if not specified
-zreader = zreader.Zreader("reddit_data.zst", chunk_size=8192)
+reader = zreader.Zreader("reddit_data.zst", chunk_size=8192)
 
 # Read each line from the reader
-for line in zreader.readlines():
+for line in reader.readlines():
     obj = json.loads(line)
     print (obj['author'], obj['subreddit'], sep=",")
 
